1290ec68360fe8eb042ba13e039446a8d496c345
xlisden <belen140904@gmail.com>
2024-12-02 17:37:49 -0500

    >

--- src/main/java/com/example/demo/controllers/MetodosController.java
+++ src/main/java/com/example/demo/controllers/MetodosController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.Persona;
 
 @Controller
 @RequestMapping("/MetodosController") //el nombre de la ruta del controlador (en este caso, la ruta raiz, porque cada metodo puede tener su ruta)
@@ -15,7 +20,21 @@ public class MetodosController {
 	}
 	
 	@GetMapping("/Metodo2")
-	public ModelAndView Metodo2() {
-		return new ModelAndView("vista2");
+	public ModelAndView Metodo2() { //se puede agregar o referenciar un objeto
+		ModelAndView mav = new ModelAndView("vista2"); //esta direccionando a la vista
+		mav.addObject("personas", getPersonas());
+		return mav;
+	}
+	
+	private List<Persona> getPersonas(){
+		List<Persona> personas = new ArrayList<Persona>();
+		personas.add(new Persona("Java Spring", "987654321"));
+		personas.add(new Persona("Java C++", "956874412"));
+		personas.add(new Persona("Python", "987654321"));
+		personas.add(new Persona("Spring Boot", "963258741"));
+		personas.add(new Persona("Java JPA", "931478562"));
+		personas.add(new Persona("Typescript", "987654123"));
+		personas.add(new Persona("CSharp", "951236874"));
+		return personas;
 	}
 }
